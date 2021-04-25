3a454810914cc825ea58a2c1264a54c97508c724
Sasikala Magesh <magesh_Laptop>
2021-04-26 00:01:26 +0530

    added python script

---
+++ python.py
@@ -0,0 +1,4 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  4 2021, 13:27:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("this is my fisrt scripts")
+this is my fisrt scripts
